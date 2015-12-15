dbb05119d46607842e0a92376c62136c6fe95d33
xspirit92 <x-spirit-alex@mail.ru>
2015-12-15 19:42:32 +0300

    изменил connectionString

--- Forecast/Controllers/HomeController.cs
+++ Forecast/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace Forecast.Controllers
     {
         public ActionResult Index()
         {
+            var roleRepository = new RoleRepository();
             return View();
         }
 
